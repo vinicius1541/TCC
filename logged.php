29e08d74e9859c00f61e56c31739ebe6892c178a
Vinicius Bernardo <vinicius-bernado2011@hotmail.com>
2020-03-17 23:03:58 -0300

    Passo 3: o rodapé foi criado no caminho Login-OdontoMonicao/view/footer.php

--- logged.php
+++ logged.php
@@ -3,3 +3,6 @@ include('view/header.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<?php
+include('view/footer.php');
+?>
---
+++ view/footer.php
@@ -0,0 +1,8 @@
+
+<footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Copyright &copy; 2020</small>
+    </div>
+  </footer>
+</body>
+</html>
